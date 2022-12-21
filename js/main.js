47a06a9ebb60459a02b6b50b347226c41667e179
Felipe Carvalho <felipe_pc7@hotmail.com>
2022-12-21 18:16:58 -0300

    finish countdown script

--- js/main.js
+++ js/main.js
@@ -1,25 +1,23 @@
-const days = document.querySelector('#days')
-const hour = document.querySelector('#hour')
-const minutes = document.querySelector('#minutes')
-const seconds = document.querySelector('#seconds')
-
-const timer = () => {
-    seconds.innerHTML = Number(seconds.innerHTML) - 1
-
-    if(seconds.innerHTML === '0') {
-        minutes.innerHTML = Number(minutes.innerHTML) - 1
-        seconds.innerHTML = 60
-    }
-
-    if(minutes.innerHTML === '0') {
-        hour.innerHTML = Number(hour.innerHTML) - 1
-        minutes.innerHTML = 59
-    }
-
-    if(hour.innerHTML === '0') {
-        days.innerHTML = Number(days.innerHTML) - 1
-        hour.innerHTML = 24
-    }
+const setTwoDigit = (arg) => {
+    return arg > 9 ? arg : '0'+arg
 }
 
-setInterval(timer, 1000)
+
+const update = () => {
+    const from = new Date()
+    const to = new Date('dec 26 2022 23:59:59')
+    const diff = to - from
+    const days = setTwoDigit(Math.floor(diff/1000/60/60/24))
+    const hours = setTwoDigit(Math.floor(diff/1000/60/60) % 24)
+    const minutes = setTwoDigit(Math.floor(diff/1000/60) % 60)
+    const seconds = setTwoDigit(Math.floor(diff/1000) % 60)
+
+    document.querySelector('#days').innerHTML = days
+    document.querySelector('#hour').innerHTML = hours
+    document.querySelector('#minutes').innerHTML = minutes
+    document.querySelector('#seconds').innerHTML = seconds
+}
+
+const interval = setInterval(update, 1000)
+
+
